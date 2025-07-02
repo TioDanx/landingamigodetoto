6ffb9e3065a6f5ddbcc4231e4ac191ca7c21ba4f
Daniel Campuzano <yesdanlocooo@gmail.com>
2025-07-02 16:31:52 -0300

    added pixel 2

--- app/layout.js
+++ app/layout.js
@@ -82,7 +82,7 @@ fbq('track', 'PageView');`}
           <img
             height="1"
             width="1"
-            style="display:none"
+            style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=749963750755701&ev=PageView&noscript=1"
           />
         </noscript>
